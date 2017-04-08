616aa2eadc7e1bb6e8cd3fb75e17c669eaffc4b4
cjw <chol0501@nate.com>
2017-04-08 17:10:36 +0900

    button click event

--- app/src/main/java/org/cba/im_seminar/MainActivity.java
+++ app/src/main/java/org/cba/im_seminar/MainActivity.java
@@ -1,7 +1,11 @@
 package org.cba.im_seminar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn;
+
+        btn = (Button)findViewById(R.id.today_button);
+        btn.setOnClickListener(mClickListener);
     }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener(){
+        public void onClick(View v){
+            Toast.makeText(MainActivity.this, "버튼클릭",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, ViewLogsActivity.class);
+            startActivity(intent);
+        }
+    };
 }
