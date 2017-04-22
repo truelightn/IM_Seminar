3b2459731f3056b72bfab40c27291636acf2dec6
aosi <aosi@192.168.101.112>
2017-04-22 16:05:35 +0900

    2주차

--- app/src/main/java/org/cba/im_seminar/MainActivity.java
+++ app/src/main/java/org/cba/im_seminar/MainActivity.java
@@ -1,7 +1,11 @@
 package org.cba.im_seminar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt;
+
+        bt = (Button)findViewById(R.id.today_button);
+
+        bt.setOnClickListener(mClickListener);
     }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "버튼글릭" , Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(MainActivity.this, ViewLogosActivity.class);
+
+            i.putExtra("test","test");
+
+            startActivity(i);
+
+        }
+    };
 }
---
+++ app/src/main/java/org/cba/im_seminar/ViewLogosActivity.java
@@ -0,0 +1,28 @@
+package org.cba.im_seminar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aosi on 2017. 4. 8..
+ */
+
+public class ViewLogosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view_logos);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
